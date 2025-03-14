3cdeebdca34fc2ab3e0143a4830a42f67d7d79dd
OtterLover07 <melker@willforss.se>
2025-03-14 14:36:22 +0100

    Did stuff, trying to fix more stuff

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.querySelector("div.menu-button").addEventListener("click", toggleMenu)
+
+function toggleMenu(){
+    // document.querySelector("div.menu").classList.toggle("show")
+    document.querySelector("div.menu").classList.toggle("hide")
+}
